86710b69cf748eac0e7b4ce0363d7217b8937cdb
Anandteerth Onkar <mailanandteerth@gmail.com>
2021-08-31 21:52:28 +0100

    completed test case TC7

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -12,8 +12,13 @@ describe('Regression Suite', () => {
   });
 
   it('TC 7', () => {
-    
+    cy.log('Visit event page');
+    cy.visit('/');
+    cy.log('Perform search action');
+    cy.contains('11,710').should('be.visible');
+    eventLandingPageObj.getSearchBox().clear().type(testdata.submission1);
+    eventLandingPageObj.getSearchIcon().click();
+    cy.log('Verify search result');
+    eventLandingPageObj.checkSubmissionDisplayed(testdata.submission1);
   });
-
- 
 });
--- cypress/pageObjects/event-landing.page.js
+++ cypress/pageObjects/event-landing.page.js
@@ -1,6 +1,8 @@
 class EventLandingPage {
   constructor() {
     this.pageTitle = 'ACS Spring 2020 National Meeting & Expo | Morressier';
+    this.itemInGrid =
+      '.PosterGrid__StyledPosterGridContainer-jugd3x-0 > div > div > div:nth-child(2) > a > div > span';
   }
 
   verifyPageTitle() {
@@ -16,13 +18,23 @@ class EventLandingPage {
   }
 
   getSearchBox() {
-    cy.get('input[placeholder="Search"]').eq(1).should('be.visible');
+    return cy.get('input[placeholder="Search"]').eq(1).should('be.visible');
   }
 
   getSearchIcon() {
-    cy.get('input[placeholder="Search"]').eq(1).next('svg').should('be.visible');
+    return cy
+      .get('input[placeholder="Search"]')
+      .eq(1)
+      .next('svg')
+      .should('be.visible');
   }
-  
+
+  checkSubmissionDisplayed(name) {
+    cy.get(this.itemInGrid).each(($list, index) => {
+      const item = $list.text();
+      expect(item).to.be.equal(name);
+    });
+  }
 }
 
 export default EventLandingPage;
